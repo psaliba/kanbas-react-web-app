0e7216074a80fa7d9ecc40e5a184587b07538bb7
Peter Saliba <petersaliba23@gmail.com>
2024-03-19 15:04:50 -0400

    3.4.1

---
+++ src/Labs/a5/WorkingWithArrays.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+function WorkingWithArrays() {
+    const API = "http://localhost:4000/a5/todos";
+    const [todo, setTodo] = useState({
+        id: "1", title: "NodeJS Assignment",
+        description: "Create a NodeJS server with ExpressJS",
+        due: "2021-09-09",
+        completed: false,
+    });
+    return (
+        <div>
+            <h3>Working with Arrays</h3>
+            <h4>Retrieving Arrays</h4>
+            <a className="btn btn-primary" href={API}>
+                Get Todos
+            </a>
+            <h4>Retrieving an item from an array by ID</h4>
+            <input type="number" value={todo.id}
+                onChange={(e) => setTodo({
+                    ...todo,
+                    id: e.target.value
+                })} />
+            <input type="text" value={todo.title}
+                onChange={(e) => setTodo({
+                    ...todo, title: e.target.value
+                })} />
+            <input type="text" value={todo.description}
+                onChange={(e) => setTodo({
+                    ...todo, description: e.target.value
+                })} />
+            <input type="checkbox" checked={todo.completed}
+                onChange={(e) => setTodo({
+                    ...todo, completed: e.target.checked
+                })} />
+            <h3>Updating an Item in an Array</h3>
+            <a className="btn btn-primary" href={`${API}/${todo.id}/title/${todo.title}`} >
+                Update Title to: {todo.title}
+            </a>
+            <a className="btn btn-primary" href={`${API}/${todo.id}/description/${todo.description}`} >
+                Update Description to: {todo.description}</a>
+
+            <a className="btn btn-primary" href={`${API}/${todo.id}/completed/${todo.completed}`} >
+                Update Completed to: {todo.completed.toString()}</a>
+
+            <a className="btn btn-primary" href={`${API}/${todo.id}`}>
+                Get Todo by ID
+            </a>
+
+            <h3>Filtering Array Items</h3>
+            <a className="btn btn-primary" href={`${API}?completed=true`}>
+                Get Completed Todos
+            </a>
+
+            <h3>Creating new Items in an Array</h3>
+            <a className="btn btn-primary" href={`${API}/create`}>
+                Create Todo
+            </a>
+
+            <h3>Deleting from an Array</h3>
+            <a className="btn btn-primary" href={`${API}/${todo.id}/delete`}>
+                Delete Todo with ID = {todo.id}
+            </a>
+        </div>
+    );
+}
+export default WorkingWithArrays;
